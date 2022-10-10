e66eac9a11f4a5b6749b2953ee538273111aac7f
kseniaDubova <ksushadubova@icloud.com>
2022-10-10 22:35:40 +0400

    правильное оформление

--- search_with_date.py
+++ search_with_date.py
@@ -1,5 +1,16 @@
-from datetime import date
+from datetime import date, datetime
+import re
 
 def searh_in_all(date):
     file = open("dataset.csv", "r")
-    print("dsf")
+    for row in file:
+        new_date = re.search(r"\d{2}\-\d{2}\-\d{4}", row)
+        if date==new_date:
+            print(row)
+            return 0
+    print(None)
+
+def search_in_year(date):
+    file = open()
+
+searh_in_all()
