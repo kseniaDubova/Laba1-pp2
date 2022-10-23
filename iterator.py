4e91ea8b6a2098c097d5edfe55d99746e947a823
kseniaDubova <ksushadubova@icloud.com>
2022-10-23 13:05:04 +0400

    итератор с выбором файла

--- iterator.py
+++ iterator.py
@@ -1,11 +1,24 @@
 
+from itertools import count
+
+
 class Iterator:
+
+    def __init__(self, name_of_file):
+        self.name_of_file=name_of_file
+        self.counter =0
+        self.list =[]
+        file = open(self.name_of_file, "r")
+        for row in file:
+            self.list.append(row)
+        file.close
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        num = self.num
-        self.num += 1
-        return num
-
+        if self.counter<len(self.list):
+            tmp= self.list[self.counter]
+            self.counter+=1
+            return tmp
+        else:  raise StopIteration
