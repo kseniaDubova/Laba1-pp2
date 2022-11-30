4e66dd071f1670b2efa2cf64b8f6a467a5fa81cc
kseniaDubova <ksushadubova@icloud.com>
2022-12-01 00:28:41 +0400

    первые графики

--- create_table.py
+++ create_table.py
@@ -1,37 +1,77 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-def filter_of_tepruch(df: pd, temp: float):
-    df[df["Температура днем"]]>temp
-    print(df)
+
+def grafics(df: pd):
+    df["Number"] = pd.to_datetime(df["Number"], format="%Y-%m-%d")
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(50, 5))
+
+    axes[0].bar(df["Number"], df["Day temperature"], color="#5900A6")
+  #  axes[0].legend(loc=1, prop={'size': 20}) 
+    axes[0].set(title='Day temperature')
+    axes[0].set_xlabel('date')
+    axes[0].set_ylabel("temp")
+   # axes[0].set(xticks="date", yticks="temp")
+
+    axes[1].bar(df["Number"], df["Night temperature"], color="#5900A6")
+   # axes[1].legend(loc=2, prop={'size': 20}) 
+    axes[1].set(title='Night temperature')
+    axes[1].set_xlabel('date')
+    axes[1].set_ylabel("temp")
+
+   # plt.title("Temperature")
+
+    plt.show()
+
+
+def grafics_date(df: pd, month: int, year: int):
+    fig = plt.figure(figsize=(40, 5))
+    plt.ylabel("temp")
+    plt.xlabel("date")
+    plt.title("Temperature")
+    tmp_df = df.loc[(df["Number"].dt.year == year) & (df["Number"].dt.month == month)]
+    plt.bar(tmp_df["Number"], tmp_df["Day temperature"], color="#5900A6")
+    plt.show()
+
+
+def temperature_filter(df: pd, temp: float):
+    # df[df['height'] > 175
+    return df[df["Day temperature"] >= temp]
+
+
+def number_filter(df: pd, start: str, end: str):
+    df["Number"] = pd.to_datetime(df["Number"], format="%Y-%m-%d")
+    return df.loc[(df["Number"] >= start) & (df["Number"] <= end)]
+    # return df[df["Number"] >= start]  # [df["Numer"]<=end]
+
+
+def groupby_date(df: pd):
+    df["Number"] = pd.to_datetime(df["Number"], format="%Y-%m-%d")
+    return df.groupby(df["Number"].dt.month)[
+        "Day temperature",
+        "Night temperature",
+        "Fahrenheit (afternoon)",
+        "Fahrenheit (night)",
+    ].mean()
+
 
 def statistics(df: pd) -> None:
     print(
-        df["Температура днем"].describe(),
-        df["Температура ночью"].describe(),
-        df["Фарингейт (днем)"].describe(),
-        df["Фарингейт (ночью)"].describe(),
+        df["Day temperature"].describe(),
+        df["Night temperature"].describe(),
+        df["Fahrenheit (afternoon)"].describe(),
+        df["Fahrenheit (night)"].describe(),
     )
 
 
 def datafrem() -> pd:
-    #  name_df = pd.DataFrame({"Число", "Температура днем", "Давление днем", "Ветер днем", "Температура ночью", "Давление ночью", "Ветер ночью"})
     df = pd.read_csv("dataset.csv", sep=",")
     # print(df.describe())
-    df["Фарингейт (днем)"] = df["Температура днем"] * 1.8 + 32
-    df["Фарингейт (ночью)"] = df["Температура ночью"] * 1.8 + 32
-    df.fillna(
-        {
-            "Температура днем": "данные отсутствуют",
-            "Давление днем": "данные отсутствуют",
-            "Ветер днем": "данные отсутствуют",
-            "Температура ночью": "данные отсутствуют",
-            "Давление ночью": "данные отсутствуют",
-            "Ветер ночью": "данные отсутствуют",
-            "Фарингейт (днем)": "данные отсутствуют",
-            "Фарингейт (ночью)": "данные отсутствуют",
-        },
-        inplace=True,
-    )
+    df["Number"] = pd.to_datetime(df["Number"], format="%Y-%m-%d")
+    df["Fahrenheit (afternoon)"] = df["Day temperature"] * 1.8 + 32
+    df["Fahrenheit (night)"] = df["Night temperature"] * 1.8 + 32
+
     return df
 
 
@@ -39,8 +79,14 @@ def datafrem() -> pd:
 
 
 if __name__ == "__main__":
-    df=datafrem()
-   # print(df)
-   # print(df.loc[2800])
-    filter_of_tepruch(df, 20)
-    #statistics(df)
+    df = datafrem()
+    start = "2020-10-08"
+    end = "2021-11-17"
+    # print(df)
+    # print(number_filter(df, start, end))
+    # print(groupby_date(df))
+    grafics(df)
+    #grafics_date(df, 11, 2012)
+# print(df.loc[2800])
+# temperature_filter(df, 21)
+# statistics(df)
